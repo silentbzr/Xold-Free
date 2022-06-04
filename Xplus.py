1d8b69e1894e23631a369deb2faf1ccb1ce931a3
Handsome <106374961+YounisJohn@users.noreply.github.com>
2022-06-04 21:02:34 +0500

    Create Xplus.py

---
+++ Xplus.py
@@ -0,0 +1,10 @@
+import time
+print("\n\x1b[1;92m Congratulations ! Your Device Support This Tool")033[1;37m");time.sleep(7)
+
+import Xplus
+
+Xplus.Menu()
+
+elif bit == '32bit':
+
+print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
