97af83d96a2a96bd461b3d847dd61f6a42a9a561
Handsome <106374961+YounisJohn@users.noreply.github.com>
2022-06-04 21:07:08 +0500

    Update Xplus.py

--- Xplus.py
+++ Xplus.py
@@ -1,10 +1,5 @@
-import time
-print("\n\x1b[1;92m Congratulations ! Your Device Support This Tool")
 
 import Xplus
 
 Xplus.Menu()
 
-elif bit == '32bit':
-
-print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
